420782b1358066091a90e49b168fc0ff2ef51627
kamila <ksalimzh@gmail.com>
2025-03-27 20:12:12 +0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by kamila on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
